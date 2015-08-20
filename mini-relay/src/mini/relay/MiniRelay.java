295dcaf2ac614254a233837b2a9904dbbc316fcc
Hadi Modarres <modarres.zadeh@gmail.com>
2015-08-20 17:35:06 +0430

    connection acceptance code

--- mini-relay/src/mini/relay/MiniRelay.java
+++ mini-relay/src/mini/relay/MiniRelay.java
@@ -5,6 +5,15 @@
  */
 package mini.relay;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author hadi
@@ -14,6 +23,45 @@ public class MiniRelay {
     /**
      * @param args the command line arguments
      */
+    private boolean isMajorOnListen = false;
+    private InetSocketAddress listenAddress=null;
+    private InetSocketAddress forwardAddress=null;
+    public MiniRelay(InetSocketAddress listenAddress,InetSocketAddress forwardAdddress,boolean majorListen){
+        isMajorOnListen = majorListen;
+        this.listenAddress = listenAddress;
+        this.forwardAddress = forwardAdddress;
+      
+    }
+    
+    private void handleNewlyAcceptedConnection(AsynchronousSocketChannel socket){
+        
+    }
+    
+    private void startListening(){
+        try {
+            AsynchronousServerSocketChannel serverSocket = AsynchronousServerSocketChannel.open().bind(this.listenAddress);
+            serverSocket.accept(null,new CompletionHandler<AsynchronousSocketChannel, Void>() {
+
+                @Override
+                public void completed(AsynchronousSocketChannel result, Void attachment) {
+                    serverSocket.accept(null, this);
+                    
+                }
+
+                @Override
+                public void failed(Throwable exc, Void attachment) {
+                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+            });
+            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(MiniRelay.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
